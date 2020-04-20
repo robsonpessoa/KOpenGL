835993156b100143cd66ceced12866e208b66579
robsonpessoa <robson.pessoa@fpf.br>
2020-04-20 18:21:50 -0300

    Create first work assignment (Spiral Movement)

--- src/main/kotlin/br/org/robsonpessoa/Main.kt
+++ src/main/kotlin/br/org/robsonpessoa/Main.kt
@@ -1,14 +1,15 @@
 package br.org.robsonpessoa
 
-import br.org.robsonpessoa.exercises.DrawLines
+import br.org.robsonpessoa.exercises.TSpiralSpring
 import br.org.robsonpessoa.glfw.Engine
 
 fun main() {
     val app = Engine()
-    val exercise = DrawLines()
+    val exercise = TSpiralSpring()
 
-    app.setWindowConfiguration("Hello World", 600, 600)
+    app.setWindowConfiguration("Hello World", 900, 900)
     app.setOnEngineListener(exercise)
+    app.getWindow()?.setOnKeyListener(exercise)
 
     app.run()
 }
---
+++ src/main/kotlin/br/org/robsonpessoa/exercises/TSpiralSpring.kt
@@ -0,0 +1,86 @@
+package br.org.robsonpessoa.exercises
+
+import br.org.robsonpessoa.Spring
+import br.org.robsonpessoa.glfw.*
+import br.org.robsonpessoa.utils.toBuffer
+import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL20.glGetUniformLocation
+import org.lwjgl.opengl.GL20.glUniformMatrix4fv
+
+class TSpiralSpring : Engine.EngineListener, OnKeyListener {
+
+    companion object {
+        private const val POSITION = "position"
+        private const val TRANSFORMATION = "mat_transformation"
+        private const val DIMENSION = 2
+    }
+
+    private val spring = Spring()
+
+    override fun onLoadProgramSettings(settings: ProgramSettings) {
+        configureShaders(settings)
+        configureVertices(settings)
+    }
+
+    override fun onDraw(window: Window, program: Program) {
+        GL11.glClearColor(0f, 0f, 0f, 1.0f)
+
+        val loc = glGetUniformLocation(program.id, TRANSFORMATION)
+        val transformation = spring.getTransformation()
+
+        glUniformMatrix4fv(
+            loc, true,
+            transformation.toFloatBuffer()
+        )
+
+        program[POSITION]?.let {
+            GL11.glDrawArrays(
+                GL11.GL_LINE_STRIP,
+                0,
+                it.capacity() / DIMENSION
+            )
+        }
+    }
+
+    private fun configureVertices(settings: ProgramSettings) {
+        settings.setData(POSITION) {
+            spring.ctrlPoints.toBuffer()
+        }
+    }
+
+    private fun configureShaders(settings: ProgramSettings) {
+        settings.addShader(
+            VertexShaderBuilder()
+                .code(
+                    """
+                            attribute vec2 ${POSITION};
+                            uniform mat4 mat_transformation;
+                            void main(){
+                                gl_Position = mat_transformation * vec4(${POSITION},0.0,1.0);
+                            }
+                            """.trimIndent()
+                )
+                .build()
+        )
+
+        settings.addShader(
+            FragmentShaderBuilder()
+                .code(
+                    """
+                            void main(){
+                                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+                            }
+                        """.trimIndent()
+                )
+                .build()
+        )
+    }
+
+    override fun onKeyPressed(key: Int, scanCode: Int, action: Int, mods: Int) {
+        if (scanCode == 116 && action == 1) {
+            spring.pull()
+        } else if (scanCode == 116 && action == 0) {
+            spring.leave()
+        }
+    }
+}
